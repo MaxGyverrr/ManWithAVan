42ad9bc66a5db91ae7c37f87707c68dc0e2a1d44
Max William <maxwilliam.infor@gmail.com>
2023-05-11 22:26:03 +0100

    Links between pages have been created

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,15 +1,37 @@
 import { ComponentFixture, TestBed ,waitForAsync} from '@angular/core/testing';
 import { LoginPage } from './login.page';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(LoginPage);
+    imports: [
+      IonicModule.forRoot(),
+      AppRoutingModule
+    ]
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
+
+  router = TestBed.get(Router);
+
+  it('should go to home page on login', () => {
+    spyOn(router, 'navigate');
+    component.login();
+    expect(router.navigate).toHaveBeenCalledWith(['home']); //call the home page
+  })
+
+  it('should go to register page on register', () => {
+    spyOn(router, 'navigate');
+    component.register();
+    expect(router.navigate).toHaveBeenCalledWith(['register']); //call the register page
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/pages/register/register.page.spec.ts
+++ src/app/pages/register/register.page.spec.ts
@@ -1,15 +1,31 @@
 import { ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import { RegisterPage } from './register.page';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
   let fixture: ComponentFixture<RegisterPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(RegisterPage);
+    imports: [
+      IonicModule.forRoot(),
+      AppRoutingModule
+    ]
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
+
+  router = TestBed.get(Router);
+
+  it('should go to home page on register', () => {
+    spyOn(router, 'navigate');
+    component.register();
+    expect(router.navigate).toHaveBeenCalledWith(['home']); // call the home page
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();

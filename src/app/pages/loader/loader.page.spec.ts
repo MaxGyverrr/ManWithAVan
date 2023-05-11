fb7c7ad31a32fde795754ac2faae4012f0d30a31
Max William <maxwilliam.infor@gmail.com>
2023-05-11 20:44:56 +0100

    Loading page running when app started

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,15 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 import { LoaderPage } from './loader.page';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(LoaderPage);
+    imports: [
+      IonicModule.forRoot(),
+      AppRoutingModule
+    ]
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
+
+  router = TestBed.get(Router);
+
+  it('should go to loginpage after load', fakeAsync(() =>{
+    spyOn(router, 'navigate');
+    component.ngOnInit();
+    tick(1500);
+    expect(router.navigate).toHaveBeenCalledWith(['login']);
+  }))
 
   it('should create', () => {
     expect(component).toBeTruthy();
